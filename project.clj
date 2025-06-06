bf13146ffb20cff34c59d3a67c9c64cae00dbc72
Lasse Määttä <lasse.olavi.maatta@gmail.com>
2025-06-06 15:27:59 +0300

    Update deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/lassemaatta/murmeli"
   :license {:name "European Union Public Licence v. 1.2"
             :url  "https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12"}
-  :dependencies [[org.clojure/clojure "1.12.0"]
+  :dependencies [[org.clojure/clojure "1.12.1"]
                  [org.clojure/tools.logging "1.3.0"]
-                 [org.mongodb/mongodb-driver-sync "5.4.0"]
+                 [org.mongodb/mongodb-driver-sync "5.5.0"]
                  [prismatic/schema "1.4.1"]]
   :target-path "target/%s"
   :deploy-repositories [["clojars"  {:url           "https://clojars.org/repo"
@@ -26,7 +26,7 @@
                              :jvm-opts       ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"]}
              :repl          {:jvm-opts ["-Dmurmeli.repl=true"]}
              :gen-doc-tests {:test-paths   ^:replace ["target/test-doc-blocks/test"]
-                             :dependencies [[com.github.lread/test-doc-blocks "1.1.20"]]}}
+                             :dependencies [[com.github.lread/test-doc-blocks "1.2.21"]]}}
   :aliases  {"run-doc-tests" ^{:doc "Generate, then run, tests from doc code blocks"}
              ["with-profile" "+gen-doc-tests" "do"
               ["run" "-m" "lread.test-doc-blocks" "gen-tests" "--platform" "clj" "README.md"]
