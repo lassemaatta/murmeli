56eb872a52e19510913303c945baab0259c5b058
Lasse Määttä <lasse.olavi.maatta@gmail.com>
2025-11-28 18:27:48 +0200

    Update dependencies
    Changes in docker required updating testcontainers

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/lassemaatta/murmeli"
   :license {:name "European Union Public Licence v. 1.2"
             :url  "https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12"}
-  :dependencies [[org.clojure/clojure "1.12.2"]
+  :dependencies [[org.clojure/clojure "1.12.3"]
                  [org.clojure/tools.logging "1.3.0"]
-                 [org.mongodb/mongodb-driver-sync "5.5.1"]
+                 [org.mongodb/mongodb-driver-sync "5.6.1"]
                  [prismatic/schema "1.4.1"]]
   :target-path "target/%s"
   :deploy-repositories [["clojars"  {:url           "https://clojars.org/repo"
@@ -13,15 +13,16 @@
                                      :username      :env/clojars_username
                                      :password      :env/clojars_password}]]
   :profiles {:dev           {:dependencies   [;; Testcontainers for mongodb
+                                              [org.testcontainers/testcontainers "2.0.2"]
                                               [clj-test-containers "0.7.4"]
                                               [org.testcontainers/mongodb "1.17.6"]
                                               ;; Generative testing with spec
-                                              [org.clojure/test.check "1.1.1"]
+                                              [org.clojure/test.check "1.1.2"]
                                               ;; Matcher for tests
                                               [nubank/matcher-combinators "3.9.2"]
                                               ;; Logging
                                               [org.slf4j/slf4j-api "2.0.17"]
-                                              [ch.qos.logback/logback-classic "1.5.18"]]
+                                              [ch.qos.logback/logback-classic "1.5.21"]]
                              :plugins        [[lein-eftest "0.6.0"]]
                              :eftest         {:multithread? true}
                              :resource-paths ["test-resources"]
